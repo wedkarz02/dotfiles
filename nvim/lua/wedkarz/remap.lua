d186c5ff9172bf3e95c6266273c36b57a4d74753
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-02-02 17:27:52 +0100

    removed vscode stuff from nvim config

--- nvim/lua/wedkarz/remap.lua
+++ nvim/lua/wedkarz/remap.lua
@@ -1,13 +1,6 @@
-local vscode = require("vscode-neovim")
 
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-if vim.g.vscode then
-    -- vscode stuff here 
-    -- vim.keymap.set("n", "<leader>w", vscode.call('workbench.action.files.save'))
-else
-    -- normal nvim stuff here
-    vim.keymap.set("n", "<leader>w", vim.cmd.write)
-end
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
