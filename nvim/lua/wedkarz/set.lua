213a3c6f23394b1c7c6acc7bc7d22d873b981681
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-02-09 15:31:03 +0100

    nvim -> yank now uses clip.exe

--- nvim/lua/wedkarz/set.lua
+++ nvim/lua/wedkarz/set.lua
@@ -1,6 +1,16 @@
 
 vim.g.mapleader = " "
 
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+        callback = function()
+            vim.fn.system("clip.exe", vim.fn.getreg('"'))
+        end,
+    })
+end
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
